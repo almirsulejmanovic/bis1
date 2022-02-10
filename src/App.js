527e554ef5ebd71c5935397d18697d36dc279f30
Almir Sulejmanovic <16432927+almirsulejmanovic@users.noreply.github.com>
2022-02-10 15:14:48 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className='app'>
+      <div className='row'>
+        <div className='col1'>
+          <h1>ONE</h1>
+        </div>
+        <div className='col2'>
+          <div className='col2-animation'>
+            <h1>TWO</h1>
+          </div>
+        </div>
+        <div className='col3'>
+          <div className='col3-animation'>
+            <h1>THREE</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
